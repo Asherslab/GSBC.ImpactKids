0e9dd5e89679f52a6bd569f37a953067c661a989
Asher Parry <Asher.Parry@qube.com.au>
2025-10-23 13:27:46 +1000

    add user feedback for is a person has already recited a verse once

--- GSBC.ImpactKids.Shared.Contracts/Entities/MemoryVerses/MemorisationEntry.cs
+++ GSBC.ImpactKids.Shared.Contracts/Entities/MemoryVerses/MemorisationEntry.cs
@@ -4,18 +4,19 @@ namespace GSBC.ImpactKids.Shared.Contracts.Entities.MemoryVerses;
 public class MemorisationEntry
 {
     public required Guid Id { get; set; }
-    
+
     public required Guid PersonId      { get; set; }
     public required Guid MemoryVerseId { get; set; }
     public required Guid ServiceId     { get; set; }
 
-    public bool VerseRecited         { get; set; }
-    public bool FiveDollaryDoosGiven { get; set; }
-    public bool OneDollaryDooGiven   { get; set; }
+    public bool VerseRecited              { get; set; }
+    public bool VerseHasBeenRecitedBefore { get; set; }
+    public bool FiveDollaryDoosGiven      { get; set; }
+    public bool OneDollaryDooGiven        { get; set; }
 
-    public Person? Person { get; set; }
+    public Person?      Person      { get; set; }
     public MemoryVerse? MemoryVerse { get; set; }
-    public Service? Service { get; set; }
+    public Service?     Service     { get; set; }
 
     public static string BuildSubscription(
         Guid? memorisationEntryId = null
