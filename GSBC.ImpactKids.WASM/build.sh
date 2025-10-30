76b93c5b00641e93bb9114d306ddf96fffe67cdf
Asher Parry <Asher.Parry@qube.com.au>
2025-10-30 16:14:45 +1000

    attempt to put api url in Release json

--- GSBC.ImpactKids.WASM/build.sh
+++ GSBC.ImpactKids.WASM/build.sh
@@ -3,4 +3,6 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
+
+EXPORT ASPNETCORE_ENVIRONMENT=Release
 ./dotnet/dotnet publish -c Release -o output
