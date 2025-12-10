6ae070a792cba0e73c38ba3e57ba0ed30585c0b4
Asher Parry <Asher.Parry@qube.com.au>
2025-12-10 21:32:39 +1000

    small date time fixes

--- GSBC.ImpactKids.WASM/Features/Calendar/Models/CalendarEvent.cs
+++ GSBC.ImpactKids.WASM/Features/Calendar/Models/CalendarEvent.cs
@@ -5,4 +5,5 @@ public class CalendarEvent
     public required DateTime Date  { get; set; }
     public required string   Name  { get; set; }
     public          string?  Color { get; set; }
+    public          string?  Href  { get; set; }
 }
